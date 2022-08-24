ca6511de98cae2c8844e686ee5266d0330b6297b
Сергей Васильченко <svasilchenko@MacBook-Air-Sergej.local>
2022-08-24 21:21:06 +0300

    add test

--- test_google_finds_selene.py
+++ test_google_finds_selene.py
@@ -1,7 +1,8 @@
 from selene.support.shared import browser
-from selene import have, be
+from selene import be, have
 
 
-browser.open('https://google.com/ncr')
-browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-browser.element('[id=search]').should(have.text('Selene'))
+def test():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
