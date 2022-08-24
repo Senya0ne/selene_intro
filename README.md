7ff1260db453381b358ac458e6541a2177175e61
Сергей Васильченко <svasilchenko@MacBook-Air-Sergej.local>
2022-08-24 21:14:49 +0300

    add selene test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # selene_intro
+
+### For install Selene use command:
+```pip install selene --pre```
+
+### 
+1. ```python3 - m venv```
+2. ```pip install -r requirements.txt```
+
---
+++ test_google_finds_selene.py
@@ -0,0 +1,7 @@
+from selene.support.shared import browser
+from selene import have, be
+
+
+browser.open('https://google.com/ncr')
+browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+browser.element('[id=search]').should(have.text('Selene'))
